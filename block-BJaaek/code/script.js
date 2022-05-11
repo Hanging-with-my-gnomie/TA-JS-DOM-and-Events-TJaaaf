5b3a3a6961e3d098343c18ee90d253829ed20aec
Rajnish kumar <mr.rajnish2000@gmail.com>
2022-05-11 10:22:00 +0530

    block-Bjaaek

---
+++ block-BJaaek/code/script.js
@@ -0,0 +1,8 @@
+
+
+let box1 = document.querySelector(".first");
+let box2 = document.querySelector(".second");
+let banner = document.querySelector(".banner");
+box1.addEventListener('click',function(){
+    banner.backgroundcolor = red;
+})
